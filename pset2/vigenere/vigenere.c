2a7c11b35196c97ecee22e69dfdf8c1a1d863a22
Sitora Urazova <sitoraurazova@gmail.com>
2018-12-03 23:40:09 +0000

    Edited Vigenere

--- pset2/vigenere/vigenere.c
+++ pset2/vigenere/vigenere.c
@@ -2,41 +2,52 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
-
-// {
-//     string s = get_string("First Name: ");
-//     for(int i = 0; i < strlen(s); i++)
-//     {
-//         //makes the ASCII codes for a string
-//         printf("%c %i\n", s[i], (int) s[i]);
-//     }
-// }
+#include <stdlib.h>
 
 
- int main(int argc, char* argv[])
+
+int main(int argc, string argv[])
 {
-    int i;
-    int key = atoi(argv[1]);
-    int result;
-    char str[60];
-
-for (i = 0; i < argc; i++)
-{
-    printf("argv[%d] = %s\n", i, argv[i]);
-}
-
- if (argc != 2)
+    if(argc != 2)
     {
-        printf("You need to enter a key\n");
+        printf("Usage: ./vigenere k\n");
         return 1;
     }
- else
-    {
 
-        if (key == 0)
-        {
-            printf("Wrong key!.\n");
-            return 1;
-        }
+    string key = (argv[1]);
+    int keyLength = strlen(key);
+    int keyCode = 0;
+    for (int j = 0; j < keyLength; j++)
+    if (!isalpha(key[j]))
+    {
+        printf("Keys may only use letters. Use an acceptable key value, \n");
+        return 1;
     }
+
+    // Encryption
+    string p = get_string("plaintext: ");
+
+    printf("ciphertext: ");
+        for(int i = 0, length = strlen(p); i < length; i++)
+        {
+            if (islower(p[i]))
+            {
+                printf("%c", (p[i] - 'a' + tolower(key[keyCode]) - 'a') % 26 + 'a');
+                keyCode = (keyCode + 1) % keyLength;
+            }
+
+            else if (isupper(p[i]))
+
+            {
+               printf("%c", (p[i] - 'A' + toupper(key[keyCode]) - 'A') % 26 + 'A');
+                keyCode = (keyCode + 1) % keyLength;
+            }
+            else
+            {
+                printf("%c", p[i]);
+            }
+        }
+
+        printf("\n");
+        return 0;
 }
