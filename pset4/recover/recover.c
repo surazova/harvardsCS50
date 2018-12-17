ad5a4ab571179a180dd75f53ecc0262e5e053ad7
Sitora Urazova <sitoraurazova@gmail.com>
2018-12-17 18:34:38 +0000

    Completed recover

--- pset4/recover/recover.c
+++ pset4/recover/recover.c
@@ -2,27 +2,67 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-// string is changed to char *argv
-int maiin(int argc, cahr *argv[])
+int main(int argc, char *argv[])
 {
-    // check for only 1 command line argument
-    //FILE *file = fopen(argv[1], "r");
+    // Enter only 1 command line argument
     if (argc != 2)
-    {
-        fprintf(stderr, "Please enter file name.\n");
-        return 1;
-    }
-    //Open the file entered into the command line
-FILE *file = fopen(argv[1], "r");
-//If the file does not exist, throw an error
-if (file == NULL)
 {
-fprintf(stderr, "Could not open %s.\n", argv[1]);
-return 1;
+
+    fprintf(stderr, "Please enter a file name to open.\n");
+    return 1;
 }
 
-}
+    // File that is entered will open
+    FILE *file = fopen(argv[1], "r");
 
+    //If the file does not exist, error
+    if (file == NULL)
+    {
+    fprintf(stderr, "Could not open %s.\n", argv[1]);
+    return 1;
+    }
+
+    // Picture output
+    FILE *img = NULL;
+    // buffer and filename arrays
+
+    unsigned char buffer[512];
+    char filename[8];
+    //Set counter for filename
+    int counter = 0;
+    //Set flag
+    bool flag = false;
+    //Read the file
+    while (fread(buffer, 512, 1, file) == 1)
+    {
+    //Check if we are at the beginning of a JPEG
+    if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
+    {
+    //Close current JPEG, so we can start reading the next
+    if (flag == true)
+    {
+    fclose(img);
+    }
+    //Condition for found JPEG
+    else
+    {
+    flag = true;
+    }
+    sprintf(filename, "%03i.jpg", counter);
+    img = fopen(filename, "w");
+    counter++;
+    }
+    if (flag == true)
+    {
+    fwrite(&buffer, 512, 1, img);
+    }
+    }
+    //Close all files
+    fclose(file);
+    fclose(img);
+    //Success
+    return 0;
+}
 
 // iterate through the array
 // look throug the JPEG signatures
