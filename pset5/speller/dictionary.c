9190e079d201f8ad88a9a6007e76cf3a92dcac98
Sitora Urazova <sitoraurazova@gmail.com>
2019-01-14 19:13:35 +0000

    Update for dictionary.c

--- pset5/speller/dictionary.c
+++ pset5/speller/dictionary.c
@@ -12,7 +12,106 @@
 
 #include "dictionary.h"
 
-#define HASHTABLE_SIZE 1985
+#define ALPHABET_SIZE 26 //26 lowercase and 26 uppercase letters
+
+// Parameters
+#define false 0
+#define true 1
+
+struct trie tree;
+
+struct trie_node
+{
+    char *value;
+    struct trie_node *children[ALPHABET_SIZE * 2]; // 2= 1 set of 26 uppercase, 1 set of 26 lowercase
+};
+
+struct trie
+{
+    struct trie_node root;
+};
+
+// Change a character to an integer (a-z =  0 - 25 || A-Z = 26 - 51)
+int letterInt(char letter)
+{
+    if (letter >= 'A' && letter <= 'Z')
+    {
+        return letter - 'A' + 26;  // 26-51
+    }
+    else if (letter >= 'a' && letter <= 'z')
+    {
+        return letter - 'a' // 0-25
+    }
+    return -1; // keep going
+}
+
+// Defines an invalid character within a word (based on its index)
+void wrongWord(const char *word, int index)
+{
+    printf(" the word: \"%s\"\n", word);
+    printf("    ");
+    int i;
+    for (i = 0; i < index; i++)
+    {
+        printf(" ");
+    }
+    printf("^\n");
+}
+
+// Puts a words into the trie table
+int trieAdd(struct trie_node *node, const char *word, char *description)
+{
+    int i;
+    for (i = 0; i < strlen(word); i++)
+    {
+        int letter = letterInt(word[i]);
+        if (letter == -1)
+        {
+            // If the character in the string is invalid
+            printf("The word was not inserted into the trie because it was invalid \n");
+            wrongWord(word, i);
+            printf(" description: \"s\"\n", description);
+            return false;
+        }
+        // Setting up Trie table (node is parent, letter is children)
+        struct trie_node *parent = node;
+        node = node->children[letter];
+
+        if (!node)
+        {
+            node = malloc(sizeof(struct trie_node));
+            parent->children[letter] = node;
+        }
+    }
+
+    // Allocating memory to each character, not word (saves memory)
+    int len = strlen(description);
+    node->value = malloc(len +1);
+    strncopy(node->value, description, len);
+    return true;
+}
+
+// Moving through Trie Table from node (root) and finds string of word description based on letters (children)
+char *trie_get(struct trie_node *node, const char *word)
+{
+    int i;
+    for (i = 0; i < strlen(word); i++
+    {
+        int letter = letterInt(word[i]);
+        if (letter == -1)
+        {
+            return false; // word found
+        }
+
+        node = node->children[letter];
+        if(!node)
+        {
+            return false; // word not found
+        }
+    }
+    return node->value;
+}
+
 
 // Create a pointer for a linked list
 typedef struct node
