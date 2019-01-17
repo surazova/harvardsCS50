2406143ec31235f08f1693f5fa6dc0915abe4173
Sitora Urazova <sitoraurazova@gmail.com>
2019-01-17 18:59:10 +0000

    Speller complete

--- pset5/speller/dictionary.h
+++ pset5/speller/dictionary.h
@@ -1,33 +1,9 @@
-// // Declares a dictionary's functionality
-
-// #ifndef DICTIONARY_H
-// #define DICTIONARY_H
-
-// #define MAX_WORD_SIZE   40
-// #define MAX_DESC_SIZE  200
-
-// void dictionaryInit();
-
-// int dictionaryRead(const char * filename);
-
-// int dictionaryLook(const char * word, char * meaning);
-
-// // // Prototypes
-// // bool check(const char *word);
-// // bool load(const char *dictionary);
-// // unsigned int size(void);
-// // bool unload(void);
-
-
-// #endif
-
 #ifndef DICTIONARY_H
 #define DICTIONARY_H
 
 #include <stdbool.h>
 
 // maximum length for a word
-// (e.g., pneumonoultramicroscopicsilicovolcanoconiosis)
 #define LENGTH 45
 
 /**
