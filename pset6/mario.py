549c607be0859f6b0b38642756f1df89fc4a0204
Sitora Urazova <sitoraurazova@gmail.com>
2019-02-13 18:47:50 +0000

    Added comments

--- pset6/mario.py
+++ pset6/mario.py
@@ -6,10 +6,14 @@ from cs50 import get_int
 # User determines height
 while True:
     height = get_int("What is the height of the pyramid? ")
+
+    # Height must be no greater than 23
     if height >= 0 and height < 24:
         break
 
-# Print a pyramid of #
+# Print a pyramid of # if i(integer) is between 1 and 23
 for i in range(height):
+        # Aligns the pyramid to the right
         print(" " * (height - i - 1), end="")
+        # Top stack is always ##
         print("#" * (i + 2))
