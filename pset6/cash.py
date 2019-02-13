0e26d354d983e0d9089e515ee4ccd48bc2ff4468
Sitora Urazova <sitoraurazova@gmail.com>
2019-02-13 18:57:14 +0000

    Completed cash in python: Determening the number of coins the user will receive

--- pset6/cash.py
+++ pset6/cash.py
@@ -1,1 +1,31 @@
 # Implement a program that calculates the minimum number of coins required to give the user change
+
+# Using numbers with decimals: Need float
+from cs50 import get_float
+
+coins = 0
+
+# Ask the user for the amount of change (make sure it's a positive number)
+while True:
+    change = get_float("How much change do you need? ")
+    if change > 0:
+        break
+
+# Change value gets changed into an integer without decimals
+change = round(int(change * 100))
+
+# Diving the previous variable change to see how many coins the user gets
+# ! Start with the largest coin first!
+
+coins = change // 25
+change %= 25
+
+coins += change // 10
+change %= 10
+
+coins += change // 5
+change %= 5
+
+coins += change // 1
+# Get the amount of coins
+print(coins)
