d1f8544fd734cfe728ebfa3b624d43c8b099704d
Sitora Urazova <sitoraurazova@gmail.com>
2019-02-13 19:17:57 +0000

    Added caesar in python: implement a program that encrypts messages

--- pset6/caesar.py
+++ pset6/caesar.py
@@ -1,25 +1,38 @@
 # Implement a program that encrypts messages using Caesar’s cipher
+# Implementation: python3 caesar.py 5
 
 import sys
 from cs50 import get_string
 
-# Cypher needs only one argument
+def main():
+    # Cypher needs only one argument
 
-if len(sys.argv) == 1:
-    print("Please add one argument.")
+    if len(sys.argv) == 1:
+        print("Please add one argument.")
 
-# Change the string into an int
-key = int(sys.argv[1])
+    # Change the string into an int
+    key = int(sys.argv[1])
 
-# Ask the user for a string
-plain = get_string("Enter a string: ")
+    # Ask the user for a string
+    plain = get_string("Enter a string: ")
 
-# Print out the cypher
-print("ciphertext: ", end="")
+    # Print out the cypher
+    print("ciphertext: ", end="")
 
-for char in plain:
-    # Uppercase letter
-    if char.isupper(:
-        print("{}".format(chr((ord(char) - ord("A") + key) % 26 + ord("A"))), end="")
+    # char(c) will change
+    for c in plain:
+        # Uppercase letter
+        if c.isupper():
+            print("{}".format(chr((ord(c) - ord("A") + key) % 26 + ord("A"))), end="")
 
-    # Lowercase letter
+        # Lowercase letter
+        elif c.islower():
+            print("{}".format(chr((ord(c) - ord("a") + key) % 26 + ord("a"))), end="")
+
+        # All others
+        else:
+            print("{}".format(c), end="")
+
+
+if __name__ == "__main__":
+    main()
